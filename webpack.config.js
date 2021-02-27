d1638c56736eda75b9ddbd8a5754c754192e9d15
Daniel <daniel.brz2009@gmail.com>
2021-02-27 19:21:14 -0300

    git generic commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,20 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/server.js',
+  entry: path.resolve(__dirname, './src/server.js'),
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, './dist'),
     filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['*', '.js'],
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
-        loader: 'babel-loader',
+        test: /\.(js)$/,
         exclude: /node_modules/,
+        use: ['babel-loader'],
       },
     ],
   },
