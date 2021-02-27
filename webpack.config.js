ebfb1b4b2b5bcc82dbf2afd336a4741c773e1df3
daniel <daniel.brz2009@gmail.com>
2021-02-27 00:24:31 -0300

    prepared to production ({[??????]})

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  entry: './src/server.js',
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
