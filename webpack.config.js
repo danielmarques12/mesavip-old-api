c47de2c1affafa431bf4e5037bcff657223ab745
Daniel <daniel.brz2009@gmail.com>
2021-02-27 19:07:47 -0300

    git generic commit

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   devtool: 'inline-source-map',
   entry: './src/server.js',
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
   module: {
