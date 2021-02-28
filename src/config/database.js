47fc2435194c0d98415561c23ea81aadb6591d16
daniel <daniel.brz2009@gmail.com>
2021-02-28 04:18:58 -0300

    Generic commit

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,11 @@
 module.exports = {
   dialect: 'postgres',
-  // dialectOptions: {
-  //   ssl: {
-  //     require: true,
-  //     rejectUnauthorized: false,
-  //   },
-  // },
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   username: process.env.DB_USER,
