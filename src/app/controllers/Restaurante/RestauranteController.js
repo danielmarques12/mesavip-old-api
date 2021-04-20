f4f911d1c3a27a1eab651941b06195611d1c643f
daniel <daniel.brz2009@gmail.com>
2021-04-20 17:18:07 -0300

    Changes in the restaurant info queries

--- src/app/controllers/Restaurante/RestauranteController.js
+++ src/app/controllers/Restaurante/RestauranteController.js
@@ -6,47 +6,25 @@ class RestauranteController {
 
     const restaurante = await db.connection.query(
       `SELECT
-      r.sobre, r.telefone, r.site,
       u.nome AS nome,
+      r.sobre, r.telefone,
       c.nome AS culinaria,
-      e.bairro, e.cidade, e.estado, e.cep, e.logradouro, e.numero, e.complemento
-      FROM restaurantes AS r
-      INNER JOIN usuarios u ON u.id = r.restaurante_id
+      e.bairro, e.cidade, e.estado, e.cep, e.logradouro, e.numero, e.complemento,
+      cast(avg(n.nota) as decimal(10,1)) AS media, count(n.nota) AS totaldeavaliacoes
+      FROM usuarios AS u
+      INNER JOIN restaurantes r ON u.id = r.restaurante_id
       INNER JOIN culinarias c ON c.id = r.culinaria_id
       INNER JOIN enderecos e ON e.usuario_id = r.restaurante_id
-      WHERE r.restaurante_id = :restaurante_id;`,
+      INNER JOIN notas n ON n.restaurante_id = r.restaurante_id
+      WHERE r.restaurante_id = :restaurante_id
+      GROUP BY r.id,u.id, c.id, e.id;`,
       {
         replacements: { restaurante_id },
         type: db.connection.QueryTypes.SELECT,
       }
     );
 
-    const media = await db.connection.query(
-      `SELECT
-      cast(avg(notas.nota) as decimal(10,1)) AS media
-      FROM notas
-      GROUP BY notas.restaurante_id;`,
-      {
-        replacements: { restaurante_id },
-        type: db.connection.QueryTypes.SELECT,
-      }
-    );
-
-    const quantidade = await db.connection.query(
-      `SELECT
-      count(notas.nota)
-      FROM notas
-      WHERE notas.restaurante_id = restaurante_id;`,
-      {
-        replacements: { restaurante_id },
-        type: db.connection.QueryTypes.SELECT,
-      }
-    );
-
-    return response.json({
-      restaurante,
-      avaliacao: { media: media[0].media, quantidade: quantidade[0].count },
-    });
+    return response.json(restaurante[0]);
   }
 
   async index(request, response) {
@@ -55,13 +33,16 @@ class RestauranteController {
       u.id, u.nome,
       c.nome as culinaria,
       e.bairro,
-      f.path
+      f.path,
+      cast(avg(n.nota) as decimal(10,1)) AS media
       FROM usuarios u
       INNER JOIN restaurantes r on u.id = r.restaurante_id
       INNER JOIN culinarias c on c.id = r.culinaria_id
       INNER JOIN enderecos e on u.id = e.usuario_id
       INNER JOIN files f on u.id = f.usuario_id
-      WHERE f.type = 'lista';`,
+      INNER JOIN notas n on n.restaurante_id = r.restaurante_id
+      WHERE f.type = 'lista'
+      GROUP BY u.id, c.id, e.id, f.id;`,
       {
         type: db.connection.QueryTypes.SELECT,
       }
