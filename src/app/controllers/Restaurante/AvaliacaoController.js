66f7b75bd0e569ee8d5e3eecf986e86e260ab9a7
daniel <daniel.brz2009@gmail.com>
2021-04-20 00:24:59 -0300

    Corrected the Schedule Query

--- src/app/controllers/Restaurante/AvaliacaoController.js
+++ src/app/controllers/Restaurante/AvaliacaoController.js
@@ -5,13 +5,15 @@ class AvalicaoController {
     const restaurante_id = request.params.id;
 
     const avaliacoes = await db.connection.query(
-      `SELECT coment.id, coment.comentario, coment."createdAt" AS data,
-        notas.nota, 
-        cliente.nome AS cliente
-        FROM comentarios AS coment
-        INNER JOIN usuarios AS cliente ON coment.cliente_id = cliente.id
-        INNER JOIN notas ON notas.cliente_id = coment.cliente_id
-        WHERE coment.restaurante_id = :restaurante_id;`,
+      `SELECT
+      coment.id, coment.comentario, coment."createdAt" AS data,
+      notas.nota,
+      cliente.nome AS cliente
+      FROM comentarios AS coment
+      INNER JOIN usuarios AS cliente ON cliente.id = coment.cliente_id
+      INNER JOIN notas ON notas.cliente_id = cliente.id
+      WHERE coment.restaurante_id = :restaurante_id
+      AND notas.restaurante_id = :restaurante_id;`,
       {
         replacements: { restaurante_id },
         type: db.connection.QueryTypes.SELECT,
